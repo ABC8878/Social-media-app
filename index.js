7dab387e5166320655d03891a450540c269dff07
Jitendra Junjariya <jitendrachoudharyiit@gmail.com>
2024-01-01 19:34:13 +0530

    demo changes

---
+++ index.js
@@ -0,0 +1,56 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import AuthRoute from './Routes/AuthRoute.js'
+import UserRoute from './Routes/UserRoute.js'
+import PostRoute from './Routes/PostRoute.js'
+import cors from "cors";
+import jwt  from "jsonwebtoken";
+// Routes
+
+const app = express();
+app.use(cors())
+
+dotenv.config();
+
+// Middleware
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
+const middleware=async (req,res,next)=>{
+let token=req.headers['authorization'];
+if(token){
+ token=token.split(' ')[1];
+ await jwt.verify(token, 'shhhhh',(err,valid)=>{
+    if(err){
+      res.status(401).send({result:"please provide valid token"});
+    }
+    else{
+      next();
+    }
+  });
+}
+else{
+  res.status(403).send({result:"please send token"});
+}  
+}
+
+
+
+  // usage of routes
+  app.use('/auth', AuthRoute)
+  app.use('/user',middleware, UserRoute)
+  app.use('/post',middleware, PostRoute)
+
+mongoose
+  .connect(process.env.MONGO_DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(process.env.PORT, () =>
+      console.log(`Listening at ${process.env.PORT}`)
+    )
+  )
+  .catch((error) => console.log(error));
